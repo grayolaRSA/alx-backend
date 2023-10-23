660b76e719d1234595f6cb78eb6b1007684c335a
grayolaRSA <gtndanga@gmail.com>
2023-10-23 19:21:28 +0200

    update

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""module for index range function"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """function to return pagination indexes"""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
