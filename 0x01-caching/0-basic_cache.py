6fee3bc9cce89a29ec89cc22695ca1f072ad2715
grayolaRSA <gtndanga@gmail.com>
2023-10-24 21:14:48 +0200

    update

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -12,7 +12,9 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if key is not None or item is not None:
+        if key is None or item is None:
+            pass
+        else:
             self.cache_data[key] = item
 
     def get(self, key):
