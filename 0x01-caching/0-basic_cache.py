e232b5c2adc6ff8a85a95d67ed8b22e2da9f31d7
grayolaRSA <gtndanga@gmail.com>
2023-10-24 21:05:44 +0200

    update

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" BasicCaching module
+"""
+
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache class"""
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+            
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key, None)
