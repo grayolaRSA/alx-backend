ec2232bf02e24683077de19b490191bb93677bce
grayolaRSA <gtndanga@gmail.com>
2023-10-31 22:17:07 +0200

    update

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,14 +2,14 @@
 """module for Flask app"""
 
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def home():
     """simple app home endpoint"""
-    return 'Hello World'
+    return render_template('templates/0-index.html')
 
 
 if __name__ == '__main__':
