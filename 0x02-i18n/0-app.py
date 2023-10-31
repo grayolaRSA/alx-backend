6235ff46248bc2ed6ac7343fb8b0ca65d7dea068
grayolaRSA <gtndanga@gmail.com>
2023-10-31 21:14:15 +0200

    update

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""module for Flask app"""
+
+
+from flask import Flask, render_template
+from flask_babel import Babel, _
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """simple app home endpoint"""
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
