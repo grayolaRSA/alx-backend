c2cc553cd518d33a7832914eabaa473f5fbe3251
grayolaRSA <gtndanga@gmail.com>
2023-10-31 21:21:00 +0200

    update

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     """simple app home endpoint"""
-    return render_template('templates/index.html')
+    return render_template('templates/0-index.html')
 
 
 if __name__ == '__main__':
