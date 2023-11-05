db510ab196da7b8716323e677ec766823201a981
grayolaRSA <gtndanga@gmail.com>
2023-11-05 14:39:06 +0200

    update

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -2,7 +2,11 @@
 """
 Flask app
 """
-from flask import Flask, render_template, request
+from flask import (
+    Flask,
+    render_template,
+    request
+)
 from flask_babel import Babel, _, gettext
 
 
