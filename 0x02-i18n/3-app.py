533ee3fe8af31ccf897ecb16e4e425c7322c5142
grayolaRSA <gtndanga@gmail.com>
2023-11-05 12:54:34 +0200

    update

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""module to setup a basic Flask app"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _, gettext
+
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+
+class Config:
+    """configuration class for Babel setup"""
+
+    LANGUAGES: list = ["en", "fr"]
+
+    BABEL_DEFAULT_LOCALE: str = "en"
+    BABEL_DEFAULT_TIMEZONE: str = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """function to get users preferred language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def home() -> str:
+    """function for simple app route with an html template"""
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    """initiate app"""
+    app.run()
