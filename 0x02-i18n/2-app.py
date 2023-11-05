5e4beae444f6c43d51032cc861afb36767be77b0
grayolaRSA <gtndanga@gmail.com>
2023-11-05 12:07:19 +0200

    update

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python3
 """
-module to setup a basic Flask app
+Flask app
 """
+from flask import (
+    Flask,
+    render_template,
+    request
+)
+from flask_babel import Babel
 
 
-from flask import Flask, render_template, request
-from flask_babel import Babel, _
+class Config(object):
+    """
+    Configuration for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-
-babel = Babel(app)
-
-
-class Config:
-    """
-    configuration class for Babel setup
-    """
-
-    LANGUAGES: list = ["en", "fr"]
-
-    BABEL_DEFAULT_LOCALE: str = "en"
-    BABEL_DEFAULT_TIMEZONE: str = "UTC"
-
-
 app.config.from_object(Config)
+babel = Babel(app)
 
 
 @babel.localeselector
@@ -36,7 +33,7 @@ def get_locale():
 
 
 @app.route('/', strict_slashes=False)
-def home() -> str:
+def index() -> str:
     """
     Handles / route
     """
@@ -44,5 +41,4 @@ def home() -> str:
 
 
 if __name__ == "__main__":
-    """initiate app"""
     app.run(port="5000", host="0.0.0.0", debug=True)
