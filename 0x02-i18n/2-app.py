4f1d212297cd75d4f68acb403f1bfe66f4124f3f
grayolaRSA <gtndanga@gmail.com>
2023-11-05 12:03:12 +0200

    update

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -25,7 +25,9 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
-    """function to get users preferred language"""
+    """
+    function to get users preferred language
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
