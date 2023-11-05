6eba2438b801ada5736b78c690e9b14a87be1059
grayolaRSA <gtndanga@gmail.com>
2023-11-05 13:23:53 +0200

    update

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,40 +1,44 @@
 #!/usr/bin/env python3
-"""module to setup a basic Flask app"""
+"""
+Flask app
+"""
+from flask import (
+    Flask,
+    render_template,
+    request
+)
+from flask_babel import Babel
 
 
-from flask import Flask, render_template, request
-from flask_babel import Babel, _, gettext
+class Config(object):
+    """
+    Configuration for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-
-babel = Babel(app)
-
-
-class Config:
-    """configuration class for Babel setup"""
-
-    LANGUAGES: list = ["en", "fr"]
-
-    BABEL_DEFAULT_LOCALE: str = "en"
-    BABEL_DEFAULT_TIMEZONE: str = "UTC"
-
-
 app.config.from_object(Config)
+babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
-    """function to get users preferred language"""
+    """
+    Select and return best language match based on supported languages
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
-def home() -> str:
-    """function for simple app route with an html template"""
+def index() -> str:
+    """
+    Handles / route
+    """
     return render_template('3-index.html')
 
 
 if __name__ == "__main__":
-    """initiate app"""
-    app.run()
+    app.run(port="5000", host="0.0.0.0", debug=True)
