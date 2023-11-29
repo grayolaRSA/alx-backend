6969f57977ca4f74a145122f617f472cd0485e15
grayolaRSA <gtndanga@gmail.com>
2023-11-29 21:59:02 +0200

    update

--- 0x03-queuing_system_in_js/7-job_creator.js
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -48,40 +48,30 @@ const jobs = [
 const kue = require('kue');
 const queue = kue.createQueue();
 
-let totalJobs = 0;
-let completedJobs = 0;
+// let totalJobs = 0;
+// let completedJobs = 0;
 
 function updatePercentage(totalJobs, completedJobs) {
     // Track the total number of jobs and the number of completed jobs
     const percentage = totalJobs > 0 ? ((completedJobs / totalJobs) * 100).toFixed(2) : 0;
     return percentage;
 }
-
-export function jobCreate(jobs) {
-
-  
-    for (const job of jobs) {
+for (const job of jobs) {
       const notificationJob = queue.create('push_notification_code_2', {phoneNumber: job.phoneNumber, message: job.message});
+      
+      
       notificationJob.on('enqueue', () => {
       console.log(`Notification job created: ${notificationJob.id}`);
-      totalJobs++;
-      console.log(`total jobs: ${totalJobs}`)
+      // totalJobs++;
+      // console.log(`total jobs: ${totalJobs}`)
       // updatePercentage();
       });
   
-  // push_notification_code.process(JOB_ID, (err, push_notification_code) => {
-  //     if (err) {
-  //     console.error('Notification job failed');
-  //     } else {
-  //         console.log('Notification job completed');
-  //     }
-  // });
-  
       notificationJob.on('complete', () => {
           console.log(`Notification job ${notificationJob.id} completed`);
-          completedJobs++;
-          console.log(`jobs completed: ${completedJobs}`);
-          const percentage = updatePercentage(totalJobs, completedJobs);
+          // completedJobs++;
+          // console.log(`jobs completed: ${completedJobs}`);
+          // const percentage = updatePercentage(totalJobs, completedJobs);
           console.log(`Notification job ${notificationJob.id} ${percentage}% complete`);
       });
   
@@ -89,16 +79,20 @@ export function jobCreate(jobs) {
           console.error(`Notification job ${notificationJob.id}  failed: ${error}`);
       });
 
+      notificationJob.on('progress', () => {
+        console.log(`Notification job ${job.progress}% complete`);
+      });
+
       notificationJob.save();
     }
 
     // Exit gracefully when the job is completed or failed
-    queue.process('push_notification_code_2', (job, done) => {
+    // queue.process('push_notification_code_2', (job, done) => {
     // Simulate a successful job completion
     // In a real scenario, you would perform the actual work here
-    console.log(`Processing job ${job.id}`);
-    done();
-    });
+    // console.log(`Processing job ${job.id}`);
+    // done();
+    // });
   
     // Gracefully shut down the queue when the process is terminated
     process.on('SIGTERM', () => {
@@ -107,7 +101,5 @@ export function jobCreate(jobs) {
         process.exit(0);
         });
     });
-}
 
-jobCreate(jobs);
 
---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,25 @@
+const kue = require('kue');
+const queue = kue.createQueue();
+
+const blackListed = ['4153518780', '4153518781'];
+
+
+const sendNotification = (phoneNumber, message, job, done) => {
+
+  // const jobProgress = queue.create('push_notification_code_2', {phoneNumber, message}).progress(0, 100);
+  job.progress(0, 100);
+
+  if (blackListed.includes(phoneNumber)) {
+    const errorMessage = `Phone number ${phoneNumber} is blacklisted`;
+    // Fail the job with an error message
+    done(new Error(errorMessage));
+  } else {
+    job.progress(50, 100);
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+  }
+}
+
+ queue.process('push_notification_code_2', 2, (job, done) => {
+    const { phoneNumber, message } = job.data;
+    sendNotification(phoneNumber, message, job, done);
+  });
