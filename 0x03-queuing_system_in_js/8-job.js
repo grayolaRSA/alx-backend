7ee74dd2732440e89e0d8ec60f410befce27637a
grayolaRSA <gtndanga@gmail.com>
2023-11-29 22:58:35 +0200

    update

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,38 @@
+const kue = require('kue');
+const queues = kue.createQueue();
+
+// const jobs = [];
+
+const createPushNotificationsJobs = (jobs, queue) => {
+    if (!Array.isArray(jobs)) {
+        throw new Error('Jobs is not an array');
+    }
+
+    queue = queues;
+    const notificationCode = queue.create('push_notification_code_3', { phoneNumber: '', message: '' });
+
+    for (const job of jobs) {    
+
+        notificationCode.on('enqueue', () => {
+            console.log(`Notification job created: ${notificationCode.id}`);
+        });
+
+        notificationCode.on('complete', () => {
+            console.log(`Notification job ${notificationCode.id} completed`);
+        });
+
+        notificationCode.on('failed', (error) => {
+            console.log(`Notification job ${notificationCode.id} failed: ${error}`);
+        });
+
+        notificationCode.on('progress', (progress) => {
+            console.log(`Notification job ${notificationCode.id} ${progress}% complete`);
+        });
+
+        // Save the job to the queue
+        notificationCode.save();
+    }
+}
+
+module.exports = createPushNotificationsJobs;
+// createPushNotificationsJobs(jobs);
